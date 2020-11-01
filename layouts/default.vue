0ab910d8cf5c477cdd48cac6e94055f2264826b9
wakusei-meron- <b0941015+gihub@gmail.com>
2020-11-01 17:46:35 +0900

    add link to header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,41 @@
 <template>
   <v-app :style="{ background: $vuetify.theme.themes.light.background }">
+    <v-app-bar class="header">
+      <v-toolbar-title>Planet MERON's Note</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <a
+        href="https://github.com/wakusei-meron-"
+        class="header-icon"
+        target="_blank"
+      >
+        <v-btn icon>
+          <v-icon>mdi-github</v-icon>
+        </v-btn>
+      </a>
+      <a
+        href="https://twitter.com/b0941015"
+        class="header-icon"
+        target="_blank"
+      >
+        <v-btn icon>
+          <v-icon>mdi-twitter</v-icon>
+        </v-btn>
+      </a>
+      <a
+        href="https://www.instagram.com/planet_meron/?hl=ja"
+        class="header-icon"
+        target="_blank"
+      >
+        <v-btn icon>
+          <v-icon>mdi-instagram</v-icon>
+        </v-btn>
+      </a>
+      <a href="mailto:b0941015@gmail.com" class="header-icon" target="_blank">
+        <v-btn icon>
+          <v-icon>mdi-email</v-icon>
+        </v-btn>
+      </a>
+    </v-app-bar>
     <v-app-bar flat>
       <v-tabs centered class="ml-n9" color="grey darken-1">
         <v-tab v-for="item in items" :key="item.url" :to="item.url">
@@ -35,7 +71,7 @@ export default Vue.extend({
 });
 </script>
 
-<style>
+<style lang="scss">
 html {
   font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
     Roboto, "Helvetica Neue", Arial, sans-serif;
@@ -58,5 +94,9 @@ html {
 .container {
   margin: 0 auto;
   min-height: 100vh;
+}
+
+a {
+  text-decoration: none;
 }
 </style>
