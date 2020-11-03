2701adb859b03dfa4bcac04b9a2321fd1aa2367f
wakusei-meron- <b0941015+gihub@gmail.com>
2020-11-03 16:24:08 +0900

    rm element-ui

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,12 +17,10 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
-    'element-ui/lib/theme-chalk/index.css'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '@/plugins/element-ui'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -42,6 +40,5 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    transpile: [/^element-ui/],
   }
 }
